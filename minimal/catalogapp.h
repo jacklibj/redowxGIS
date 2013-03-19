967cfc624483a6f4490787f1ea73959f9c8805d7
jacklibj <worinco@163.com>
2013-03-20 01:42:05 +0800

    modified from a wxGIS sample minimal
    add some config file of wxGIS

---
+++ minimal/catalogapp.h
@@ -0,0 +1,81 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        minimal.cpp
+// Purpose:     Minimal wxWidgets sample
+// Author:      Julian Smart
+// Modified by:
+// Created:     04/01/98
+// RCS-ID:      $Id$
+// Copyright:   (c) Julian Smart
+// Licence:     wxWindows licence
+/////////////////////////////////////////////////////////////////////////////
+
+// ============================================================================
+// declarations
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// headers
+// ----------------------------------------------------------------------------
+
+// For compilers that support precompilation, includes "wx/wx.h".
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+
+// for all others, include the necessary headers (this file is usually all you
+// need because it includes almost all "standard" wxWidgets headers)
+/*
+#ifndef WX_PRECOMP
+    #include "wx/wx.h"
+#endif
+*/
+
+
+
+#include <wx/dir.h>
+#include <wx/filename.h>
+#include <wx/file.h>
+#include <wx/ffile.h>
+#include <wx/datetime.h>
+#include <wx/intl.h>
+
+#include "wxGIS\Framework\config.h"
+
+#include <wx/wx.h>
+
+
+// ----------------------------------------------------------------------------
+// resources
+// ----------------------------------------------------------------------------
+
+// the application icon (under Windows and OS/2 it is in resources and even
+// though we could still include the XPM here it would be unused)
+#ifndef wxHAS_IMAGES_IN_RESOURCES
+    #include "../sample.xpm"
+#endif
+
+// ----------------------------------------------------------------------------
+// private classes
+// ----------------------------------------------------------------------------
+
+// Define a new application type, each program should derive a class from wxApp
+class wxGISCatalogApp : 
+	public wxApp
+{
+public:
+    // override base class virtuals
+    // ----------------------------
+	 //wxGISCatalogApp(void);
+	 //~wxGISCatalogApp(void);
+
+    // this one is called on application startup and is a good place for the app
+    // initialization (doing it here and not in the ctor allows to have an error
+    // return: if OnInit() returns false, the application terminates)
+    virtual bool OnInit();
+
+private:
+	wxGISAppConfig* m_pConfig;
+
+};
---
+++ minimal/wxgis/framework/config.h
@@ -0,0 +1,13 @@
+
+#include "framework.h"
+
+class WXDLLIMPEXP_GIS_FRW wxGISConfig :
+	public IGISConfig
+{
+};
+
+class WXDLLIMPEXP_GIS_FRW wxGISAppConfig :
+	public wxGISConfig
+{
+
+};
