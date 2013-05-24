aaec654b892573406ec1730562bd84d8dbce32fd
jacklibj <worinco@163.com>
2013-05-24 21:17:55 +0800

    up

---
+++ include/wxgis/carto/cartocmd.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "wxgis/framework/framework.h"
+#include "wxgis/carto/mapview.h"
+
+//------------
+// wxGISCartoMainCmd
+//----------------------
+class wxGISCartoMainCmd :
+	public ICommand
+{
+	DECLARE_DYNAMIC_CLASS(wxGISCartoMainCmd)
+public:
+	wxGISCartoMainCmd(void);
+	virtual ~wxGISCartoMainCmd(void);
+	//ICommand
+	virtual wxIcon GetBitmap(void);
+	virtual wxString GetCaption(void);
+	virtual wxString GetCategory(void);
+	virtual bool GetChecked(void);
+	virtual bool GetEnabled(void);
+	virtual wxString GetMessage(void);
+	virtual wxGISEnumCommandKind GetKind(void);
+	virtual void OnClick(void);
+	virtual bool OnCreate(IApplication* pApp);
+	virtual wxString GetTooltip(void);
+	virtual unsigned char GetCount(void);
+private:
+	IApplication* m_pApp;
+	wxImageList m_ImageList;
+	wxGISMapView* m_pMapView;
+};
+
+//----
+// wxGISCartoMainTool
+//-----
+
+class wxGISCartoMainTool :
+{
+	DECLARE_DYNAMIC_CLASS(wxGISCartoMainTool)
+};
