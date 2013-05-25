c5a6ce5056f8eb1b9f7a698a18be5114eaf2cc3e
jacklibj <worinco@163.com>
2013-05-25 23:42:57 +0800

    few add

--- src/carto/mapview.cpp
+++ src/carto/mapview.cpp
@@ -1,3 +1,14 @@
 #include "wxgis/carto/mapview.h"
 //
-#include "wxgis/display/"
+#include "wxgis/display/screendisplayplus.h"
+
+#define WAITTIME 650
+
+//------
+// wxDrawingThread
+//--------
+
+wxDrawingThread::wxDrawingThread(wxGISMapView* pView, std::vector<wxGISLayer*>& Layers) : wxThread(), m_pView(pView), m_Layers(Layers)
+{
+
+}
